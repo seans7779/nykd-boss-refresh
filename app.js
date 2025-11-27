ff68a8027a57c997d9f54ae1ed3d5202a55c614e
nzx <nzx0316@163.com>
2025-11-27 17:37:13 +0800

    feat: 刷新间隔预设调整为2小时20分钟(140分钟)，同步初始化逻辑

--- app.js
+++ app.js
@@ -95,8 +95,8 @@ class BossRefreshApp {
         let preset = '';
         if (this.BOSS_REFRESH_INTERVAL === 275) {
             preset = '275';
-        } else if (this.BOSS_REFRESH_INTERVAL === 150) {
-            preset = '150';
+        } else if (this.BOSS_REFRESH_INTERVAL === 140) {
+            preset = '140';
         } else {
             preset = 'custom';
             const hours = Math.floor(this.BOSS_REFRESH_INTERVAL / 60);
